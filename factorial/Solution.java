c18112657d7161c6eded264bdb45f8cec504b770
Ulrich VACHON <uvachon@gmail.com>
2017-08-17 01:28:52 +0200

    Add factorial implementation

---
+++ factorial/Solution.java
@@ -0,0 +1,37 @@
+import java.io.*;
+import java.util.*;
+import java.util.function.*;
+
+/**
+ * Factorial implementation
+ * 
+ * Implement the factorial algorithm with recursion.
+ */ 
+public class Solution {
+
+  public static void main(String[] args) {
+    new Solution().should_calculate_factorial();
+  }
+
+  public void should_calculate_factorial() {
+    Factorial factorial = new Factorial();
+
+    assertThat(factorial.calculate(8), 40320);
+  }
+
+  class Factorial {
+
+    public int calculate(int element) {
+      return (element == 0) ? 1 : element * calculate(element - 1);
+    }
+  }
+
+  public static void assertThat(int test, int expect) {
+    String testMethod = Thread.currentThread().getStackTrace()[2].toString();
+
+    if(test != expect) {
+      throw new IllegalStateException("Assertion failed in " + testMethod + ". Expected=" + expect + " found=" + test);
+    }
+    System.out.println(testMethod + " passed!");
+  }
+}
