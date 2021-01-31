fa7027b77860e750b409a1272f778064fcd52964
Ulrich VACHON <uvachon@gmail.com>
2021-02-01 00:07:30 +0100

    Refactoring code aims for removing the helper class
    Signed-off-by: Ulrich VACHON <uvachon@gmail.com>

--- src/main/java/net/reservoircode/data_structures/my_hashtable/HashTable.java
+++ src/main/java/net/reservoircode/data_structures/my_hashtable/HashTable.java
@@ -100,28 +100,4 @@ public class HashTable<T> {
             this.next = null;
         }
     }
-
-    public static class Person {
-        private final String firstName;
-        private final String lastName;
-        private final String phone;
-
-        public Person(String firstName, String lastName, String phone) {
-            this.firstName = firstName;
-            this.lastName = lastName;
-            this.phone = phone;
-        }
-
-        public String getFirstName() {
-            return firstName;
-        }
-
-        public String getLastName() {
-            return lastName;
-        }
-
-        public String getPhone() {
-            return phone;
-        }
-    }
 }
--- src/test/java/net/reservoircode/data_structures/my_hashtable/HashTableTest.java
+++ src/test/java/net/reservoircode/data_structures/my_hashtable/HashTableTest.java
@@ -54,7 +54,7 @@ class HashTableTest {
     @Test
     void should_return_null_if_no_element_found() {
         // Given, When
-        HashTable<HashTable.Person> hashtable = new HashTable<>();
+        HashTable<Person> hashtable = new HashTable<>();
 
         // Then
         assertThat(hashtable.count()).isEqualTo(0);
@@ -64,13 +64,13 @@ class HashTableTest {
     @Test
     void should_put_element_without_collision() {
         // Given
-        HashTable<HashTable.Person> hashtable = new HashTable<>(4, logger);
+        HashTable<Person> hashtable = new HashTable<>(4, logger);
 
         // When
-        hashtable.put("id1", new HashTable.Person("User1", "Name1", "0102030405"));
-        hashtable.put("id2", new HashTable.Person("User2", "Name2", "0102030405"));
-        hashtable.put("id3", new HashTable.Person("User3", "Name3", "0102030405"));
-        hashtable.put("id4", new HashTable.Person("User4", "Name4", "0102030405"));
+        hashtable.put("id1", new Person("User1", "Name1"));
+        hashtable.put("id2", new Person("User2", "Name2"));
+        hashtable.put("id3", new Person("User3", "Name3"));
+        hashtable.put("id4", new Person("User4", "Name4"));
 
         // Then
         assertThat(hashtable.count()).isEqualTo(4);
@@ -85,12 +85,12 @@ class HashTableTest {
     @Test
     void should_put_element_with_collision() {
         // Given
-        HashTable<HashTable.Person> hashtable = new HashTable<>(2, logger);
+        HashTable<Person> hashtable = new HashTable<>(2, logger);
 
         // When
-        hashtable.put("id1", new HashTable.Person("User1", "Name1", "0102030405"));
-        hashtable.put("id2", new HashTable.Person("User2", "Name2", "0102030405"));
-        hashtable.put("id3", new HashTable.Person("User3", "Name3", "0102030405"));
+        hashtable.put("id1", new Person("User1", "Name1"));
+        hashtable.put("id2", new Person("User2", "Name2"));
+        hashtable.put("id3", new Person("User3", "Name3"));
 
         // Then
         assertThat(hashtable.count()).isEqualTo(3);
@@ -101,5 +101,23 @@ class HashTableTest {
         verify(logger, times(0)).debug("Collision when putting key: {}", "id2");
         verify(logger, times(1)).debug("Collision when putting key: {}", "id3");
         verify(logger, times(1)).debug("Collision when getting key: {}", "id3");
+    }
+
+    private static class Person {
+        private final String firstName;
+        private final String lastName;
+
+        public Person(String firstName, String lastName) {
+            this.firstName = firstName;
+            this.lastName = lastName;
+        }
+
+        public String getLastName() {
+            return lastName;
+        }
+
+        public String getFirstName() {
+            return firstName;
+        }
     }
 }
