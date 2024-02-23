fbf1447c4651bc7aa2586fe299b71fcf6704394d
Ulrich VACHON <uvachon@gmail.com>
2024-02-23 11:16:07 +0100

    selection sort: Java implementation
    Signed-off-by: Ulrich VACHON <uvachon@gmail.com>

---
+++ src/main/java/net/reservoircode/sort/selection_sort/SelectionSort.java
@@ -0,0 +1,20 @@
+package net.reservoircode.sort.selection_sort;
+
+public class SelectionSort {
+
+    public int[] sort(int[] array) {
+        for (int i = 0; i < array.length - 1; i++) {
+            int min = i;
+
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[j] < array[min]) {
+                    min = j;
+                }
+            }
+            int tmp = array[i];
+            array[i] = array[min];
+            array[min] = tmp;
+        }
+        return array;
+    }
+}
---
+++ src/test/java/net/reservoircode/sort/selection_sort/SelectionSortTest.java
@@ -0,0 +1,46 @@
+package net.reservoircode.sort.selection_sort;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.random.RandomGenerator;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class SelectionSortTest {
+
+    @Test
+    public void should_return_sorted_array() {
+        assertThat(new SelectionSort().sort(new int[]{12, 9, 3, 7, 14, 11}))
+                .isEqualTo(new int[]{3, 7, 9, 11, 12, 14});
+    }
+
+    @Test
+    public void should_return_sorted_array_with_duplicate() {
+        assertThat(new SelectionSort().sort(new int[]{12, 12, 3, 7}))
+                .isEqualTo(new int[]{3, 7, 12, 12});
+    }
+
+    @Test
+    public void should_return_sorted_array_with_many_entries() {
+        // Given
+        RandomGenerator randomGenerator = RandomGenerator.getDefault();
+
+        int[] array = new int[100_000];
+
+        for (int i = 0; i < array.length; i++) {
+            array[i] = randomGenerator.nextInt(999);
+        }
+
+        // When
+        int[] sortedArray = new SelectionSort().sort(array);
+
+        // Then
+        assertThat(sortedArray).isSorted();
+    }
+
+    @Test
+    public void should_return_unsorted_array_when_empty() {
+        assertThat(new SelectionSort().sort(new int[]{}))
+                .isEqualTo(new int[]{});
+    }
+}
